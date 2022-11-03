2551c139f2e26620e6092f9c2e9410da5191299c
ej <alujip8100@ieselcaminas.org>
2022-11-03 15:49:20 +0100

    V5

---
+++ Cosas para Entorno/src/cosa5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class cosa5 {
+    public static void main (String [] args) {
+        double valor1;
+        double valor2;
+        Scanner miTeclado = new Scanner(System.in);
+        System.out.println("Introduce valor 1");
+        valor1 = miTeclado.nextFloat();
+
+        System.out.println("Introduce valor 2");
+        valor2 = miTeclado.nextFloat();
+
+        if (valor1 > valor2 ) {
+            System.out.println (valor2);
+            System.out.println (valor1);
+        }else if(valor2 > valor1){
+            System.out.println (valor1);
+            System.out.println (valor2);
+
+        }else{
+            System.out.println("Los valores son iguales");
+        }
+    }
+}
+
